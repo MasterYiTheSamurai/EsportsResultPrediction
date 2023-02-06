062e09d837fc11d167e75c59bfa17e0d593235ac
Blaskó Gergő <blasko.gergo.hun@gmail.com>
2023-02-06 13:56:49 +0100

    Update Prepare.py
    Preserving empty future data.

--- Prepare.py
+++ Prepare.py
@@ -4,12 +4,16 @@ import shutil
 
 def function():
     print("Removing contents of future folder.")
-    folder = os.getcwd() + "/future/"
+    try:
+        folder = os.getcwd() + "/future/"
+    except:
+        os.mkdir(os.getcwd() + "/future/")
     for filename in os.listdir(folder):
         try:
             os.remove(folder + filename)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (filename, e))
+    os.mkdir(os.getcwd() + "/future/")
     print("Preparation phase is done.")
 
 

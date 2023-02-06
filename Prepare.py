b15d8d908ef92401f6aca8ae475c676e123d344d
Blaskó Gergő <blasko.gergo.hun@gmail.com>
2023-02-06 14:05:22 +0100

    Update Prepare.py

--- Prepare.py
+++ Prepare.py
@@ -4,18 +4,16 @@ import shutil
 
 def function():
     print("Removing contents of future folder.")
-    try:
-        folder = os.getcwd() + "/future/"
-    except Exception as e:
-        print(e)
-        os.mkdir(os.getcwd() + "/future/")
     folder = os.getcwd() + "/future/"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     for filename in os.listdir(folder):
         try:
             os.remove(folder + filename)
         except Exception as e:
             print('Failed to delete %s. Reason: %s' % (filename, e))
-    os.mkdir(os.getcwd() + "/future/")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     print("Preparation phase is done.")
 
 

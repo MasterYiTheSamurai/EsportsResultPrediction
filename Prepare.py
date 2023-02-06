d0823b1dfe35f3f0400c1f35880b74e67eb1a984
Blaskó Gergő <blasko.gergo.hun@gmail.com>
2023-02-06 13:49:00 +0100

    Readding future folder.

--- Prepare.py
+++ Prepare.py
@@ -6,14 +6,10 @@ def function():
     print("Removing contents of future folder.")
     folder = os.getcwd() + "/future/"
     for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
         try:
-            if os.path.isfile(file_path) or os.path.islink(file_path):
-                os.unlink(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
+            os.remove(folder + filename)
         except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+            print('Failed to delete %s. Reason: %s' % (filename, e))
     print("Preparation phase is done.")
 
 
